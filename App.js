89a7ce2d1a0638acdf80e05ca11b0267fd0ea800
Skymaster640 <skylar.TheHouseRobinson@gmail.com>
2022-05-05 22:04:25 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Homescreen from "./screens/home"
+
+export default function App() {
+  return (
+        <Homescreen/>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
